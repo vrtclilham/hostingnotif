d1f6991aec879a3a3c9c6e9ae8b483dea16c05b4
vrtclilham <214185ilham@gmail.com>
2021-12-28 23:08:46 +0700

    update

--- ta1.py
+++ ta1.py
@@ -1,7 +1,7 @@
-import pyrebase
 from pusher_push_notifications import PushNotifications
 
-config = { 
+import pyrebase
+config = {
   'apiKey': "AIzaSyA2g32ThPVN4Eop132xmPlzwKPzI_tYwxc",
   'authDomain' : "ilham-iot.firebaseapp.com",
   'databaseURL' : "https://ilham-iot-default-rtdb.firebaseio.com",
@@ -11,33 +11,29 @@ config = {
 }
 
 firebase = pyrebase.initialize_app(config)
-
 db=firebase.database()
 pn_client = PushNotifications(
-   instance_id='282b5269-42bd-4740-b141-6bfcd776d979',
-   secret_key='A3814C0D10D3E13F050905913B13B56900C84A328DE198B764878F8B06663D5E',
+instance_id='282b5269-42bd-4740-b141-6bfcd776d979',
+secret_key='A3814C0D10D3E13F050905913B13B56900C84A328DE198B764878F8B06663D5E',
 )
-
 def stream_handler(message):
-	print(message)
-	if(message['data'] is 1):
-		response = beams_client.publish(
-  			interests=['hello'],
-  			publish_body={
-  				'apns':{
-  					'aps':{
-  						'alert': 'Hello!',
-						},
-					},
-    					'fcm': {
-      						'notification': {
-       						'title': 'Security Sistem',
-        					'body': 'Sidik Jari Terakses',
-     						 },
-    					},
-  				},
-		)
-print(response['publishId'])
-
-
-my_stream1 = db.child("statusFingerprint").stream(stream_handler,None)
+  print(message)
+  if(message['data'] is 1):
+    response = pn_client.publish(
+    interests=['hello'],
+publish_body={
+'apns': {
+'aps': {
+'alert': 'Hello!',
+},
+},
+'fcm': {
+'notification': {
+'title': 'Hello',
+'body': 'Hello, world!',
+},
+},
+},
+)
+# print(response['publishId'])
+my_stream = db.child("statusFingerprint").stream(stream_handler,None)
